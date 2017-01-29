c12b255ecbbc19831f30a8064b8436c698290a33
Gitesh Koli <gkoli@apigee.com>
2017-01-28 19:33:03 -0500

    Changed the implementation to count time for curl request

--- tests/scripts/tasks/get_all_apps.php
+++ tests/scripts/tasks/get_all_apps.php
@@ -14,14 +14,21 @@ $developer_stats = [];
 foreach($_REQUEST['orgs'] as $org) {
     $time = time();
     $curl = curl_init( 'https://api.enterprise.apigee.com/v1/o/' . $org . '/apps');
-    curl_setopt_array($curl, array(
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_USERPWD => 'gitesh@gkoli.info:p0o9i8U7',
-    ));
-    $result = curl_exec($curl);
-    $developers = json_decode($result);
-    $request_info = curl_getinfo($curl);
-    $developer_stats[$org] = ['count' => count($developers), 'total_time' => $request_info['total_time'], 'timestamp' => $time];
+  $headers = [
+    'Accept: application/json',
+    'Authorization: Basic ' . base64_encode('gitesh@gkoli.info:p0o9i8U7'),
+  ];
+
+  $options = [
+    CURLOPT_HTTPHEADER => $headers,
+    CURLOPT_RETURNTRANSFER => true,
+  ];
+  curl_setopt_array($curl, $options);
+  $start = microtime(true);
+  $result = curl_exec($curl);
+  $time_elapsed_secs = microtime(true) - $start;
+  $developers = json_decode($result);
+  $developer_stats[$org] = ['count' => count($developers), 'total_time' => $time_elapsed_secs, 'timestamp' => $time];
 }
 
 $conn = new pdo('mysql:unix_socket=/cloudsql/apigee-aggregator:us-central1:aggregator;dbname=analytics_db', 'root', '');
@@ -30,6 +37,6 @@ foreach($developer_stats as $org => $info) {
     $query = "INSERT INTO analytics values('{$org}', 'all_apps', {$info['timestamp']}, '', '*', {$info['total_time']}, {$info['count']})";
     $conn->query($query);
 }
-$conn->close();
+// $conn->close();
 http_response_code(200);
 exit;
--- tests/scripts/tasks/get_all_developers.php
+++ tests/scripts/tasks/get_all_developers.php
@@ -6,7 +6,7 @@
  * Time: 2:07 PM
  */
 
-if($_SERVER['REQUEST_METHOD'] !== 'POST') {
+if(FALSE && $_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(500);
     exit;
 }
@@ -14,14 +14,21 @@ $developer_stats = [];
 foreach($_REQUEST['orgs'] as $org) {
     $time = time();
     $curl = curl_init( 'https://api.enterprise.apigee.com/v1/o/' . $org . '/developers');
-    curl_setopt_array($curl, array(
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_USERPWD => 'gitesh@gkoli.info:p0o9i8U7',
-    ));
+  $headers = [
+    'Accept: application/json',
+    'Authorization: Basic ' . base64_encode('gitesh@gkoli.info:p0o9i8U7'),
+  ];
+
+  $options = [
+    CURLOPT_HTTPHEADER => $headers,
+    CURLOPT_RETURNTRANSFER => true,
+  ];
+  curl_setopt_array($curl, $options);
+    $start = microtime(true);
     $result = curl_exec($curl);
+    $time_elapsed_secs = microtime(true) - $start;
     $developers = json_decode($result);
-    $request_info = curl_getinfo($curl);
-    $developer_stats[$org] = ['count' => count($developers), 'total_time' => $request_info['total_time'], 'timestamp' => $time];
+   $developer_stats[$org] = ['count' => count($developers), 'total_time' => $time_elapsed_secs, 'timestamp' => $time];
 }
 
 $conn = new pdo('mysql:unix_socket=/cloudsql/apigee-aggregator:us-central1:aggregator;dbname=analytics_db', 'root', '');
@@ -30,7 +37,5 @@ foreach($developer_stats as $org => $info) {
     $query = "INSERT INTO analytics values('{$org}', 'all_developers', {$info['timestamp']}, '*', '', {$info['total_time']}, {$info['count']})";
     $conn->query($query);
 }
-$conn->close();
-
 http_response_code(200);
 exit;
