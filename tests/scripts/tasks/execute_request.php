45d7239b0888560127458b711c648b549b05bca5
Gitesh Koli <gkoli@google.com>
2017-01-30 19:35:20 -0500

    Added the proxy to the testing

--- tests/scripts/tasks/execute_request.php
+++ tests/scripts/tasks/execute_request.php
@@ -9,7 +9,9 @@
 function execute_aggregator_request($org_url_mapping, $metric, $developer, $app , $count_function) {
     $all_responses = [];
     $request_stats = [];
-    foreach($org_url_mapping as $org => $url) {
+    $url = array_values($org_url_mapping)[0];
+    $apigee_proxy_url = str_replace("https://api.enterprise.apigee.com/v1", "https://gitesh-prod.apigee.net/multiorg", $url);
+    foreach($org_url_mapping + ['apigee_proxy' => $apigee_proxy_url] as $org => $url) {
         $time = time();
         $curl = curl_init( $url);
         $headers = [
@@ -38,7 +40,7 @@ function execute_aggregator_request($org_url_mapping, $metric, $developer, $app 
     }
 
     $conn = new pdo('mysql:unix_socket=/cloudsql/apigee-aggregator:us-central1:aggregator;dbname=analytics_db', 'root', '');
-var_dump($request_stats);
+
     foreach($request_stats as $org => $info) {
         $query = "INSERT INTO analytics values('{$org}', '{$metric}', {$info['timestamp']}, '{$developer}', '{$app}', {$info['total_time']}, {$info['count']})";
         $conn->query($query);
